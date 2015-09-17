edaba146862e0fb000fe02ab12f890c56b200766
Fisher Liang <swimnose@gmail.com>
2015-09-17 17:32:49 +0800

    Add article show page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,12 @@
 class ArticlesController < ApplicationController
+ 	before_action :find_article, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@articles = Article.all
 	end
 
 	def show
+
 	end
 
 	def new
@@ -31,6 +34,10 @@ class ArticlesController < ApplicationController
 
 	private
 		def article_params
-			params.require(:article).permit(:name, :content)
+			params.require(:article).permit(:title, :content)
+		end
+
+		def find_article
+			@article = Article.find(params[:id])
 		end
 end
